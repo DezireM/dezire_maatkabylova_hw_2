c46b482466f7b14a857253a6715905f93cc448b6
DezireM <deziremaatkabylova@gmail.com>
2024-03-17 22:34:05 +0600

    1.The permission method now returns a string instead of directly printing the result. 2.In the main method, the results of calling permission are printed using System.out.println

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,14 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        permission(generateRandomAge(), 10);
-        permission(generateRandomAge(), 20);
-        permission(generateRandomAge(), 25);
-        permission(generateRandomAge(), -5);
-        permission(generateRandomAge(), 32);
+        System.out.println(permission(generateRandomAge(), 10));
+        System.out.println(permission(generateRandomAge(), 20));
+        System.out.println(permission(generateRandomAge(), 25));
+        System.out.println(permission(generateRandomAge(), -5));
+        System.out.println(permission(generateRandomAge(), 32));
     }
 
-    public static void permission(int age, int temperature) {
+    public static String permission(int age, int temperature) {
         String result = "";
         if ((age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) ||
                 (age < 20 && temperature >= 0 && temperature <= 28) ||
@@ -21,7 +21,7 @@ public class Main {
         } else {
             result = "Оставайтесь дома";
         }
-        System.out.println("Возраст: " + age + ", Температура: " + temperature + " - " + result);
+        return "Возраст: " + age + ", Температура: " + temperature + " - " + result;
     }
 
     public static int generateRandomAge() {
